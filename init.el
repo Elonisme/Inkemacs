acc80d82f9f85059d49c578df3cc770ebc664d51
elonisme <elonisme@163.com>
2023-06-15 15:01:50 +0800

    update config

--- init.el
+++ init.el
@@ -53,6 +53,8 @@
 (use-package quelpa-use-package
   :ensure t)
 
+(use-package auto-package-update)
+
 (defun open-custom-post-file()
   "This functions dose finde custom-post.el."
   (interactive)
@@ -79,3 +81,16 @@
 (provide 'init)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yasnippet which-key vertico use-package-ensure-system-package undo-tree toc-org super-save smartparens shackle sdcv rime restart-emacs rainbow-delimiters quelpa-use-package pyvenv-auto py-autopep8 popper plantuml-mode pinyinlib ox-reveal ox-pandoc ox-gfm org-super-links org-roam-ui org-noter org-modern org-contrib org-auto-tangle org-appear org-ai orderless no-littering multiple-cursors minions marginalia magit-delta lsp-mode keycast htmlize helpful gnuplot fontaine flycheck fanyi embark-consult eglot ef-themes edit-indirect doom-modeline diminish diff-hl denote dashboard crux counsel corfu consult-notes company-box cdlatex cape cal-china-x avy auto-package-update all-the-icons-completion)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
